c860e0c20311993fb312e40807195f83925f99db
nashiaholloway <nashiaholloway1@gmail.com>
2020-04-25 00:25:00 -0400

    setting up socket program

--- learning_paths/complete_beginner/scripting_sockets.py
+++ learning_paths/complete_beginner/scripting_sockets.py
@@ -4,6 +4,15 @@
 
 # def calculation ex: add 900 3212 add 900 and move to port 3212; ex: minus 212 3499, subtract 212 and move on to port 3499
 def calc(op, num, port):
+    pass
 
+if __name__ == "__main__":
+    # open socket
 
-if __main__ == 
+    # set vars
+    op = "add"
+    num = 0
+    port = 0
+    
+    # start calculations
+    calc(op, num, port)
